351aca0f7a469f80383657edc10a942ab0349375
yenchia189929 <jennifer189929@gmail.com>
2021-11-24 21:09:55 +0800

    updat ftp. can use thread, upload, download, sh command in same folder

--- ftp/client/client.py
+++ ftp/client/client.py
@@ -3,44 +3,65 @@ import socket, os
 import hashlib
 
 client = socket.socket()
-client.connect(('localhost',9999))
+client.connect(('localhost',9992))
 
-def download(cmd):
-	client.send(cmd.encode()) #傳送命令，形式 get filename
-	server_response = client.recv(1024) #接收檔案大小資訊
-	print("servr response:",server_response)
-	client.send(b"ready to recv file") #傳送確認資訊。
-	file_total_size = int(server_response.decode()) #將檔案大小int化。
-	received_size = 0 #初始化接收資料大小，為0
-	filename = cmd.split()[1] #獲取檔名
-	f = open(filename + ".new","wb") #以二進位制形式寫入
-	m = hashlib.md5() #為md5準備
+def download(filename):					# 從遠端下載檔案
+	server_response = client.recv(1024) # 接收檔案大小資訊
+	client.send(b"ready to recv file") # 傳送確認資訊
+	file_total_size = int(server_response.decode()) # 檔案大小int化
+	received_size = 0 # 初始化接收資料大小，為0
+	f = open(filename,"wb") # 以二進位制形式寫入
+	m = hashlib.md5() # 為md5準備，來確保我下載和遠端的檔事是不是真的是一樣的，沒有在唬你
 
 	while received_size != file_total_size:
-	#'''下面的if判斷是用來完整接收檔案，從而避免粘包。'''
-		if file_total_size - received_size > 1024: # 要收不止一次 24 
+		if file_total_size - received_size > 1024: # 檔案太大，要收不止一次 1024
 			size = 1024 
 		else: # 最後一次了，剩多少收多少 
 			size = file_total_size - received_size 
-			print("last receive:",size) 
-		data = client.recv(size) #data只需要是一小個記憶體，大小為1k就好 
+			# print("last receive:",size) 
+		data = client.recv(size) # data只需要是一小個記憶體，大小為1k就好 
 		received_size += len(data) 
-		m.update(data) #不斷更新md5 
-		f.write(data) #不斷寫入 
+		m.update(data) # 不斷更新md5 
+		f.write(data) # 不斷寫入 
 		print(file_total_size, received_size) 
 
-	new_file_md5 = m.hexdigest() #獲取十六進位制的md5 
-	print("file recv done",received_size,file_total_size) 
+	new_file_md5 = m.hexdigest() # 獲取十六進位制的md5 
+	print("download complete") 
 	f.close()
-	server_file_md5 = client.recv(1024) #接收md5值 
-	print("server file md5:",server_file_md5) 
+	server_file_md5 = client.recv(1024) # 接收md5值 
+	print("server file md5:",server_file_md5.decode()) 	# 證明我的兩邊的檔案是一樣的
 	print("client file md5:",new_file_md5) 
 
+def upload(filename):
+	print(filename)
+	if os.path.isfile(filename):  # 判斷是否該檔名為檔案
+		f = open(filename,"rb")
+		m = hashlib.md5() # 為md5準備
+		file_size = os.stat(filename).st_size # 利用os.stat獲取檔案的大小
+		client.send( str(file_size).encode() ) # send file size
+		client.recv(1024) # 等待確認，同時可以防止粘包。
+		for line in f: # 一行一行傳送資料，同時更新md5
+			m.update(line)  # 不斷更新md5
+			conn.send(line) # 不斷送資料。
+		# print("file md5: ",m.hexdigest()) # 十六進位制的md5
+		f.close()
+		client.send(m.hexdigest().encode()) # send md5
+		print("upload complete")
+def shCMD():
+	cmd = client.recv(1024).decode()
+	print(cmd)
+
 while True:
-	cmd = input(">> :").strip() #形式 get filename
-	if len(cmd) == 0: 
+	data = input("input command: ")		# 形式 get filename
+	cmd = data.split()
+	if len(data) == 0: 
 		client.close()
 		print("client closed connection")
 		break
-	if cmd.startswith("get"):
-		download(cmd)
+	client.send(data.encode()) 
+	if cmd[0] == "DOWNLOAD":
+		download(cmd[1])
+	elif cmd[0] == "UPLOAD":
+		upload(cmd[1])
+	else:
+		shCMD()
--- ftp/server/server.py
+++ ftp/server/server.py
@@ -1,9 +1,61 @@
 #!/usr/bin/python3
 import hashlib
-import socket, os, time, _thread
+import socket, os, time, _thread, subprocess
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('localhost',9999))
+server.bind(('localhost',9992))
 server.listen(10)
+
+def download(conn, filename):
+	print(filename)
+	if os.path.isfile(filename):  # 判斷是否該檔名為檔案
+		f = open(filename,"rb")
+		m = hashlib.md5() # 為md5準備
+		file_size = os.stat(filename).st_size # 利用os.stat獲取檔案的大小
+		conn.send( str(file_size).encode() ) # send file size
+		conn.recv(1024) 
+		for line in f: # 一行一行傳送資料，同時更新md5
+			m.update(line)  # 不斷更新md5
+			conn.send(line) # 不斷送資料。
+		print("file md5",m.hexdigest()) # 十六進位制的md5
+		f.close()
+		conn.send(m.hexdigest().encode()) # send md5
+	print("send done")
+
+def upload(conn, filename):
+	server_response = conn.recv(1024) # 接收檔案大小資訊
+	conn.send(b"ready to recv file") # 傳送確認資訊
+	file_total_size = int(server_response.decode()) #檔案大小
+	received_size = 0 # 初始化接收資料大小，為0
+	f = open(filename + ".new","wb") # 以二進位制形式寫入
+	m = hashlib.md5() # 為md5準備
+
+	while received_size != file_total_size:
+		if file_total_size - received_size > 1024:
+			size = 1024 
+		else: # 最後一次了，剩多少收多少 
+			size = file_total_size - received_size 
+			print("last receive:",size) 
+		data = conn.recv(size) 
+		received_size += len(data) 
+		m.update(data) # 不斷更新md5 
+		f.write(data) # 不斷寫入 
+		print(file_total_size, received_size) 
+
+	new_file_md5 = m.hexdigest() # 獲取十六進位制的md5 
+	print("file recv done") 
+	f.close()
+	server_file_md5 = conn.recv(1024) # 接收md5值 
+
+def shCMD(cmd):
+	output = subprocess.getoutput(cmd.decode())
+	print(output)
+	if output == '':
+		print("output = none")
+		conn.send(" ".encode())
+	else:
+		conn.send(output.encode())
+	print("syscall complete")
+
 
 def clientThread(conn):
 	while True:
@@ -12,25 +64,16 @@ def clientThread(conn):
 		if not data:
 			print("client closed")
 			break
-		cmd,filename = data.decode().split()
-		print(filename)
-		if os.path.isfile(filename):  #判斷是否該檔名為檔案
-			f = open(filename,"rb")
-			m = hashlib.md5() #為md5準備
-			file_size = os.stat(filename).st_size #利用os.stat獲取檔案的大小
-			conn.send( str(file_size).encode() ) #send file size
-			conn.recv(1024) #等待確認，同時可以防止粘包。
-			for line in f: #一行一行傳送資料，同時更新md5
-				m.update(line)  #不斷更新md5
-				conn.send(line) #不斷髮送資料。
-			print("file md5",m.hexdigest()) #十六進位制的md5
-			f.close()
-			conn.send(m.hexdigest().encode()) #send md5
-		print("send done")
+		cmd = data.decode().split()
+		if cmd[0] == "DOWNLOAD":
+			download(conn, cmd[1])
+		elif cmd[0] == "UPLOAD":
+			upload(conn, cmd[1])
+		else:
+			shCMD(data)
 		
-
 while True:
-	conn,addr = server.accept()
+	conn, addr = server.accept()
 	print("connected by " + str(addr))
 	_thread.start_new_thread(clientThread, (conn, ))
 server.close()
